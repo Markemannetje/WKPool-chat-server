04dd53e67e34ba00a5ec6833d45a938c7aea01ce
Markemannetje <mj.boeijen@upcmail.nl>
2026-07-27 09:09:08 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -21,28 +21,56 @@ const io = new Server(httpServer, {
     credentials: true
   }
 });
-
 io.on('connection', (socket) => {
+
   console.log(`Gebruiker verbonden via socket: ${socket.id}`);
-  
+
   // Iedereen gaat direct in de hoofdchatbox
   socket.join('hoofd_chatbox');
 
-  // Luister naar live berichten
+
+  // -----------------------------
+  // Nieuw chatbericht
+  // -----------------------------
   socket.on('groeps_bericht', (data) => {
+
     console.log(`Live bericht ontvangen van ${data.username}: ${data.message}`);
-    
-    // Stuur direct live door naar ALLE ANDERE gebruikers in de hoofd_chatbox
+
     socket.broadcast.to('hoofd_chatbox').emit('nieuw_bericht', {
       username: data.username,
       message: data.message,
       timestamp: data.timestamp || new Date()
     });
+
   });
 
-  socket.on('disconnect', () => {
-    console.log(`Gebruiker verbroken: ${socket.id}`);
+
+  // -----------------------------
+  // Iemand is aan het typen
+  // -----------------------------
+  socket.on('typing', (data) => {
+
+    socket.broadcast.to('hoofd_chatbox').emit('typing', data);
+
   });
+
+
+  // -----------------------------
+  // Iemand is gestopt met typen
+  // -----------------------------
+  socket.on('stop_typing', () => {
+
+    socket.broadcast.to('hoofd_chatbox').emit('stop_typing');
+
+  });
+
+
+  socket.on('disconnect', () => {
+
+    console.log(`Gebruiker verbroken: ${socket.id}`);
+
+  });
+
 });
 
 // Start de server op poort 3000 (of de poort die Render toewijst)
